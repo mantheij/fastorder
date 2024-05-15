551345650d42639a92297fe7ef036426e5229611
Benet Restelica <benet.restelica@gmail.com>
2024-05-15 22:39:11 +0200

    Docs für die Backlogs

--- Frontend/src/views/ProductSettings.js
+++ Frontend/src/views/ProductSettings.js
@@ -1,16 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import {
-    Box, Button, Typography, InputBase, IconButton, Paper,
-    Drawer, Accordion, AccordionSummary, AccordionDetails,
+    Box, Button, Typography, InputBase, IconButton, Paper, Divider,
+    Accordion, AccordionSummary, AccordionDetails,
     List, ListItem, ListItemText, Checkbox, Dialog, DialogTitle,
-    DialogContent, DialogActions, Modal, TextField, InputLabel, Select, FormControl, DialogContentText
+    DialogContent, DialogActions, Modal, TextField, InputLabel, Select, FormControl, MenuItem, DialogContentText
 } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import DeleteIcon from '@mui/icons-material/Delete';
 import AddIcon from '@mui/icons-material/Add';
-import MenuItem from "@mui/material/MenuItem";
 
 const modalStyle = {
     position: 'absolute',
@@ -29,15 +28,64 @@ const modalStyle = {
     overflow: 'auto'
 };
 
-
+/**
+ * The Settings component is responsible for managing product categories and products,
+ * allowing users to add, delete, and search for products and categories.
+ */
 const Settings = () => {
+    /**
+     * State to manage the list of product categories.
+     */
     const [categories, setCategories] = useState([]);
+
+    /**
+     * State to manage the list of products grouped by category.
+     */
     const [products, setProducts] = useState({});
+
+    /**
+     * State to manage the search filter value.
+     */
     const [filter, setFilter] = useState('');
+
+    /**
+     * State to manage the set of selected product IDs.
+     */
     const [selectedProducts, setSelectedProducts] = useState(new Set());
+
+    /**
+     * State to manage the visibility of the Add Product modal.
+     */
     const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+
+    /**
+     * State to manage the visibility of the Delete Products dialog.
+     */
     const [deleteDialogOpen, setDeleteDialogOpen] = useState(false);
 
+    /**
+     * State to manage the visibility of the Add Category modal.
+     */
+    const [isAddCategoryModalOpen, setIsAddCategoryModalOpen] = useState(false);
+
+    /**
+     * State to manage the visibility of the Delete Category dialog.
+     */
+    const [deleteCategoryDialogOpen, setDeleteCategoryDialogOpen] = useState(false);
+
+    /**
+     * State to manage the data of a new category being added.
+     */
+    const [newCategory, setNewCategory] = useState({ name: '', description: '' });
+
+    /**
+     * State to manage the ID of the category to be deleted.
+     */
+    const [categoryToDelete, setCategoryToDelete] = useState('');
+
+    /**
+     * State to manage the data of a new product being added.
+     */
     const [newProduct, setNewProduct] = useState({
         name: '',
         price: '',
@@ -46,7 +94,9 @@ const Settings = () => {
         size: ''
     });
 
-
+    /**
+     * useEffect hook to load product categories and products from the server when the component mounts.
+     */
     useEffect(() => {
         axios.get('http://localhost:8080/api/productCategories')
             .then(response => {
@@ -56,6 +106,9 @@ const Settings = () => {
             .catch(error => console.error('Error loading categories:', error));
     }, []);
 
+    /**
+     * Fetches the list of products from the server and groups them by category.
+     */
     const fetchProducts = () => {
         axios.get('http://localhost:8080/api/products')
             .then(response => {
@@ -68,25 +121,84 @@ const Settings = () => {
             .catch(error => console.error('Error loading products:', error));
     };
 
+    /**
+     * Filters products by category and search filter.
+     * @param {string} categoryId - The ID of the category to filter.
+     * @returns {Array} - The filtered products.
+     */
     const filteredProducts = categoryId => {
         return products[categoryId] ? products[categoryId].filter(product => product.name.toLowerCase().includes(filter)) : [];
     };
+
+    /**
+     * Formats the price of a product to a string with a currency symbol.
+     * @param {number} price - The price of the product.
+     * @returns {string} - The formatted price.
+     */
     const formatPrice = (price) => {
         return `${price.toFixed(2).replace('.', ',')}€`;
     };
 
+    /**
+     * Opens the Add Product modal.
+     */
     const handleAddModalOpen = () => setIsAddModalOpen(true);
+
+    /**
+     * Closes the Add Product modal.
+     */
     const handleAddModalClose = () => setIsAddModalOpen(false);
 
+    /**
+     * Opens the Add Category modal.
+     */
+    const handleAddCategoryModalOpen = () => setIsAddCategoryModalOpen(true);
 
+    /**
+     * Closes the Add Category modal.
+     */
+    const handleAddCategoryModalClose = () => setIsAddCategoryModalOpen(false);
+
+    /**
+     * Opens the Delete Category dialog.
+     */
+    const handleDeleteCategoryDialogOpen = () => {
+        setDeleteCategoryDialogOpen(true);
+    };
+
+    /**
+     * Closes the Delete Category dialog.
+     */
+    const handleDeleteCategoryDialogClose = () => setDeleteCategoryDialogOpen(false);
+
+    /**
+     * Updates the state with the new product data entered by the user.
+     * @param {object} event - The input change event.
+     */
     const handleNewProductChange = (event) => {
         setNewProduct({ ...newProduct, [event.target.name]: event.target.value });
     };
 
+    /**
+     * Updates the state with the new category data entered by the user.
+     * @param {object} event - The input change event.
+     */
+    const handleNewCategoryChange = (event) => {
+        setNewCategory({ ...newCategory, [event.target.name]: event.target.value });
+    };
+
+    /**
+     * Updates the search filter state with the value entered by the user.
+     * @param {object} event - The input change event.
+     */
     const handleFilterChange = (event) => {
         setFilter(event.target.value.toLowerCase());
     };
 
+    /**
+     * Adds or removes a product from the selected products set.
+     * @param {string} productId - The ID of the product to toggle.
+     */
     const handleToggleProduct = (productId) => {
         const newSelected = new Set(selectedProducts);
         if (newSelected.has(productId)) {
@@ -97,6 +209,9 @@ const Settings = () => {
         setSelectedProducts(newSelected);
     };
 
+    /**
+     * Formats the new product data and sends a POST request to add the product to the server.
+     */
     const handleAddProduct = () => {
         const imgName = `${newProduct.name.toLowerCase().replace(/ /g, '_')}.jpg`;
         const cleanedPrice = newProduct.price.replace('€', '').replace(',', '.');
@@ -126,14 +241,38 @@ const Settings = () => {
             });
     };
 
+    /**
+     * Sends a POST request to add the new category to the server.
+     */
+    const handleAddCategory = () => {
+        axios.post('http://localhost:8080/api/productCategories', newCategory)
+            .then(response => {
+                console.log('Category added successfully:', response.data);
+                setCategories([...categories, response.data]);
+                setIsAddCategoryModalOpen(false);
+            })
+            .catch(error => {
+                console.error('Error adding category:', error);
+            });
+    };
+
+    /**
+     * Opens the Delete Products dialog.
+     */
     const handleDeleteProducts = () => {
         setDeleteDialogOpen(true);
     };
 
+    /**
+     * Closes the Delete Products dialog.
+     */
     const handleCloseDialog = () => {
         setDeleteDialogOpen(false);
     };
 
+    /**
+     * Sends DELETE requests to remove the selected products from the server and updates the state.
+     */
     const deleteSelectedProducts = () => {
         axios.all(Array.from(selectedProducts).map(productId =>
             axios.delete(`http://localhost:8080/api/products/${productId}`)
@@ -147,7 +286,25 @@ const Settings = () => {
         });
     };
 
+    /**
+     * Sends a DELETE request to remove the selected category from the server and updates the state.
+     */
+    const deleteCategory = () => {
+        axios.delete(`http://localhost:8080/api/productCategories/${categoryToDelete}`)
+            .then(() => {
+                console.log('Category deleted successfully');
+                setCategories(categories.filter(category => category.categoryId !== categoryToDelete));
+                setDeleteCategoryDialogOpen(false);
+            })
+            .catch(error => {
+                console.error('Failed to delete category', error);
+            });
+    };
 
+    /**
+     * Renders the confirmation dialog for deleting selected products.
+     * @returns {JSX.Element} - The delete confirmation dialog.
+     */
     const renderDeleteConfirmDialog = () => (
         <Dialog
             open={deleteDialogOpen}
@@ -177,6 +334,52 @@ const Settings = () => {
         </Dialog>
     );
 
+    /**
+     * Renders the confirmation dialog for deleting a category.
+     * @returns {JSX.Element} - The delete category confirmation dialog.
+     */
+    const renderDeleteCategoryConfirmDialog = () => (
+        <Dialog
+            open={deleteCategoryDialogOpen}
+            onClose={handleDeleteCategoryDialogClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+        >
+            <DialogTitle id="alert-dialog-title">{"Confirm delete category"}</DialogTitle>
+            <DialogContent>
+                <FormControl fullWidth>
+                    <InputLabel id="category-select-label">Select Category</InputLabel>
+                    <Select
+                        labelId="category-select-label"
+                        id="category-select"
+                        value={categoryToDelete}
+                        label="Select Category"
+                        onChange={(event) => setCategoryToDelete(event.target.value)}
+                    >
+                        {categories.map((category) => (
+                            <MenuItem key={category.categoryId} value={category.categoryId}>
+                                {category.name}
+                            </MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={deleteCategory} color="error" variant="contained" autoFocus>
+                    Delete
+                </Button>
+                <Button onClick={handleDeleteCategoryDialogClose} color="primary" variant="contained">
+                    Cancel
+                </Button>
+            </DialogActions>
+        </Dialog>
+    );
+
+    /**
+     * Finds a product by its ID in the products state.
+     * @param {string} productId - The ID of the product to find.
+     * @returns {object|null} - The found product or null if not found.
+     */
     const findProduct = (productId) => {
         for (const key in products) {
             if (products.hasOwnProperty(key)) {
@@ -189,13 +392,22 @@ const Settings = () => {
 
     return (
         <Box sx={{ padding: 4, maxHeight: 'calc(100vh - 150px)', overflow: 'auto' }}>
-            <Button variant="contained" startIcon={<AddIcon />} onClick={handleAddModalOpen}>
-                Add Product
-            </Button>
-            <Button variant="contained" color="error" startIcon={<DeleteIcon />} onClick={handleDeleteProducts} sx={{ ml: 2 }}>
-                Delete Products
-            </Button>
-            <Paper sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', justifyContent: 'center', mt: 2}}>
+            <Box sx={{ display: 'flex', gap: 2, justifyContent: 'space-between', width: '100%' }}>
+                <Button variant="contained" startIcon={<AddIcon />} onClick={handleAddCategoryModalOpen} sx={{ flex: 1 }}>
+                    Add Category
+                </Button>
+                <Button variant="contained" color="error" startIcon={<DeleteIcon />} onClick={handleDeleteCategoryDialogOpen} sx={{ flex: 1 }}>
+                    Delete Category
+                </Button>
+                <Divider orientation="vertical" flexItem />
+                <Button variant="contained" startIcon={<AddIcon />} onClick={handleAddModalOpen} sx={{ flex: 1 }}>
+                    Add Product
+                </Button>
+                <Button variant="contained" color="error" startIcon={<DeleteIcon />} onClick={handleDeleteProducts} sx={{ flex: 1 }}>
+                    Delete Products
+                </Button>
+            </Box>
+            <Paper sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', justifyContent: 'center', mt: 2 }}>
                 <InputBase
                     sx={{ ml: 1, flex: 1 }}
                     placeholder="Search Products"
@@ -228,10 +440,10 @@ const Settings = () => {
                                     />
                                 </ListItem>
                             ))}
-                            </List>
-                        </AccordionDetails>
-                    </Accordion>
-                ))}
+                        </List>
+                    </AccordionDetails>
+                </Accordion>
+            ))}
 
             <Modal open={isAddModalOpen} onClose={handleAddModalClose}>
                 <Box sx={modalStyle}>
@@ -239,8 +451,8 @@ const Settings = () => {
                         Add New Product
                     </Typography>
                     <TextField label="Name" name="name" fullWidth margin="normal" value={newProduct.name} onChange={handleNewProductChange} />
-                    <TextField label="Price €" name="price" fullWidth margin="normal" value={newProduct.price} onChange={handleNewProductChange} placeholder="0.00"/>
-                    <TextField label="Quantity" name="quantity" type="number" fullWidth margin="normal" value={newProduct.quantity} onChange={handleNewProductChange} placeholder="0"/>
+                    <TextField label="Price €" name="price" fullWidth margin="normal" value={newProduct.price} onChange={handleNewProductChange} placeholder="0.00" />
+                    <TextField label="Quantity" name="quantity" type="number" fullWidth margin="normal" value={newProduct.quantity} onChange={handleNewProductChange} placeholder="0" />
 
                     <FormControl fullWidth margin="normal">
                         <InputLabel id="category-label">Category</InputLabel>
@@ -270,7 +482,23 @@ const Settings = () => {
                     </Button>
                 </Box>
             </Modal>
+
+            <Modal open={isAddCategoryModalOpen} onClose={handleAddCategoryModalClose}>
+                <Box sx={modalStyle}>
+                    <Typography variant="h6" component="h2" sx={{ mb: 2 }}>
+                        Add New Category
+                    </Typography>
+                    <TextField label="Name" name="name" fullWidth margin="normal" value={newCategory.name} onChange={handleNewCategoryChange} />
+                    <TextField label="Description" name="description" fullWidth margin="normal" value={newCategory.description} onChange={handleNewCategoryChange} />
+
+                    <Button variant="contained" color="primary" onClick={handleAddCategory} sx={{ mt: 2 }}>
+                        Submit
+                    </Button>
+                </Box>
+            </Modal>
+
             {renderDeleteConfirmDialog()}
+            {renderDeleteCategoryConfirmDialog()}
         </Box>
     );
 };
--- Frontend/src/views/SettingsView.js
+++ Frontend/src/views/SettingsView.js
@@ -2,9 +2,18 @@ import React from 'react';
 import { Button } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 
+/**
+ * The Settings component provides a button to navigate to the Product Settings page.
+ */
 const Settings = () => {
+    /**
+     * useNavigate hook from react-router-dom to programmatically navigate to different routes.
+     */
     const navigate = useNavigate();
 
+    /**
+     * Handles the navigation to the Product Settings page.
+     */
     const handleNavigateToProductSettings = () => {
         navigate('/settings/product');
     };
