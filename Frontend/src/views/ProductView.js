3cedc625f466ee1b58a4a097e2be26f06377b997
Fynn-Luca Knorr <fknorr@stud.hs-heilbronn.de>
2024-05-16 00:46:31 +0200

    - Added my Backlogs

--- Frontend/src/views/ProductView.js
+++ Frontend/src/views/ProductView.js
@@ -1,103 +1,110 @@
-// Importiere erforderliche Abhängigkeiten aus React und MUI-Bibliothek
-import React, {useState, useEffect} from "react";
-import axios from 'axios'; // HTTP-Anfragenbibliothek für das Abholen von Produktdaten
-import Cookies from 'js-cookie'; // Bibliothek zur Verwaltung von Cookies
+// Import necessary dependencies from React and MUI library
+import React, { useEffect, useState } from "react";
+import axios from 'axios'; // HTTP request library for fetching product data
+import Cookies from 'js-cookie'; // Library for managing cookies
 import {
-    Container,
-    Grid,
+    Accordion,
+    AccordionDetails,
+    AccordionSummary,
+    Alert,
+    Avatar,
+    Badge,
+    Button,
     Card,
     CardActionArea,
     CardContent,
     CardMedia,
-    Typography,
-    CardActions,
-    Tabs,
-    Tab,
-    Button,
-    Fab,
-    Badge,
-    Snackbar,
-    Alert,
+    Container,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
     Drawer,
+    Fab,
+    Grid,
+    IconButton,
     List,
     ListItem,
+    ListItemAvatar,
+    ListItemIcon,
     ListItemText,
-    Select,
     MenuItem,
-    IconButton,
+    Select,
+    Snackbar,
+    Tab,
+    Tabs,
     TextField,
-    Dialog,
-    DialogTitle,
-    DialogContent,
-    DialogActions,
-    ListItemIcon, Avatar, ListItemAvatar
-} from "@mui/material"; // Material-UI-Komponenten
-// MUI-Symbole importieren
+    Typography,
+} from "@mui/material"; // Material-UI components
+// Import MUI icons
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import DeleteIcon from '@mui/icons-material/Delete';
-import {useNavigate} from "react-router-dom";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import SearchIcon from '@mui/icons-material/Search';
+import { useNavigate } from "react-router-dom";
 
-// Funktion für die Darstellung von Produktkarten und Warenkorb
+// Function for displaying product cards and cart
 const ProductView = () => {
-    // Zustände für verschiedene Daten und UI-Interaktionen
-    const [drinks, setDrinks] = useState([]); // Zustand für Produkte
-    const [uniqueDrinks, setUniqueDrinks] = useState([]); // Zustand für eindeutige Produkte (ohne Duplikate)
-    const [value, setValue] = useState(0); // Zustand für aktiven Tab-Wert
-    const [cart, setCart] = useState([]); // Zustand für Warenkorb
-    const [alertOpen, setAlertOpen] = useState(false); // Zustand für Snackbar (Benachrichtigung)
-    const [alertMessage, setAlertMessage] = useState(''); // Zustand für Nachricht der Snackbar
-    const [alertSeverity, setAlertSeverity] = useState('success'); // Zustand für Schweregrad der Snackbar-Nachricht
-    const [bottomSheetOpen, setBottomSheetOpen] = useState(false); // Zustand für die untere Blattöffnung (Auswahlgröße und Menge)
-    const [selectedProduct, setSelectedProduct] = useState({}); // Zustand für das ausgewählte Produkt
-    const [openDialog, setOpenDialog] = useState(false); // Zustand für Dialog (Warenkorbansicht)
-    const navigate = useNavigate();
+    // States for various data and UI interactions
+    const [drinks, setDrinks] = useState([]); // State for products
+    const [uniqueDrinks, setUniqueDrinks] = useState([]); // State for unique products (without duplicates)
+    const [value, setValue] = useState(0); // State for active tab value
+    const [cart, setCart] = useState([]); // State for cart
+    const [alertOpen, setAlertOpen] = useState(false); // State for Snackbar (notification)
+    const [alertMessage, setAlertMessage] = useState(''); // State for Snackbar message
+    const [alertSeverity, setAlertSeverity] = useState('success'); // State for Snackbar severity
+    const [bottomSheetOpen, setBottomSheetOpen] = useState(false); // State for bottom sheet open (selecting size and quantity)
+    const [selectedProduct, setSelectedProduct] = useState({}); // State for selected product
+    const [openDialog, setOpenDialog] = useState(false); // State for dialog (cart view)
+    const [isSearchVisible, setIsSearchVisible] = useState(false); // State for search bar visibility
+    const [searchQuery, setSearchQuery] = useState(''); // State for search query
     const [extras, setExtras] = useState('');
-    const baseURL = "http://localhost:3000/images/products/"; // Basierend auf Ihrer Serverkonfiguration
+    const navigate = useNavigate();
+    const baseURL = "http://localhost:3000/images/products/"; // Based on your server configuration
 
-
-    // Effekt für die Initialisierung von Produktdaten und des Warenkorbs
+    // Effect for initializing product data and cart
     useEffect(() => {
-        // Lade den Warenkorb aus Cookies, falls vorhanden
+        // Load cart from cookies if available
         const loadedCart = Cookies.get('cart');
         if (loadedCart) {
             setCart(JSON.parse(loadedCart));
         }
-        // Lade Produktdaten von der API
+        // Load product data from API
         axios.get("http://localhost:8080/api/products")
             .then(response => {
-                setDrinks(response.data); // Setze Produktdaten im Zustand
-                // Filtere eindeutige Produkte und setze sie im Zustand
+                setDrinks(response.data); // Set product data in state
+                // Filter unique products and set them in state
                 setUniqueDrinks(Array.from(new Set(response.data.map(drink => drink.name)))
                     .map(name => response.data.find(drink => drink.name === name)));
             })
-            .catch(error => console.error("Error loading the products:", error)); // Fehlerbehandlung bei Datenabruf
-    }, []); // Leerer Abhängigkeitsarray bedeutet einmalige Ausführung beim ersten Rendern
+            .catch(error => console.error("Error loading the products:", error)); // Error handling for data retrieval
+    }, []); // Empty dependency array means run once on initial render
 
     const handleOpenCardView = () => {
         if (cart.length === 0) {
-            setAlertMessage('Your cart is empty'); // Setzen der Fehlermeldung
-            setAlertSeverity('error'); // Setzen des Schweregrads der Snackbar auf 'error'
-            setAlertOpen(true); // Öffnen der Snackbar
+            setAlertMessage('Your cart is empty'); // Set error message
+            setAlertSeverity('error'); // Set severity of Snackbar to 'error'
+            setAlertOpen(true); // Open Snackbar
         } else {
-            navigate('/product/card'); // Navigieren zur CardView Route, wenn Warenkorb nicht leer ist
+            navigate('/product/card'); // Navigate to CardView route if cart is not empty
         }
     };
-    const handleCloseDialog = () => setOpenDialog(false); // Schließe den Dialog
+    const handleCloseDialog = () => setOpenDialog(false); // Close the dialog
 
-    // Funktion zur Behandlung des Hinzufügens eines Produkts zum Warenkorb
+    // Function for handling adding a product to cart
     const handleAddToCart = () => {
-        const newCart = [...cart, {...selectedProduct, imgName: `${selectedProduct.imgName}`, extras }];
-        console.log(newCart);  // Überprüfen Sie den neuen Warenkorb
+        const newCart = [...cart, { ...selectedProduct, imgName: `${selectedProduct.imgName}`, extras }];
+        console.log(newCart); // Check the new cart
         setCart(newCart);
         Cookies.set('cart', JSON.stringify(newCart), { expires: 7 });
         setAlertMessage('Added drink to cart');
         setAlertSeverity('success');
         setAlertOpen(true);
         setBottomSheetOpen(false);
-        setExtras(''); // Extras zurücksetzen
+        setExtras(''); // Reset extras
     };
 
-    // Funktion zur Behandlung der Öffnung des unteren Blatts (Größe und Menge auswählen)
+    // Function for handling opening bottom sheet (selecting size and quantity)
     const handleOpenBottomSheet = (product) => {
         const productWithImage = {
             ...product,
@@ -111,41 +118,41 @@ const ProductView = () => {
         setBottomSheetOpen(true);
     };
 
-    // Funktion zur Behandlung des Schließens des unteren Blatts
+    // Function for handling closing bottom sheet
     const handleCloseBottomSheet = () => {
-        setBottomSheetOpen(false); // Schließe das untere Blatt
+        setBottomSheetOpen(false); // Close bottom sheet
     };
 
-    // Funktion zur Behandlung der Größenänderung
+    // Function for handling size change
     const handleSizeChange = (event) => {
-        const newSize = event.target.value; // Neue Größe aus dem Ereignis
-        // Suche das ausgewählte Produkt mit der neuen Größe und aktualisiere es
+        const newSize = event.target.value; // New size from the event
+        // Find the selected product with the new size and update it
         const selectedSize = drinks.find(p => p.name === selectedProduct.name && p.size === newSize);
-        setSelectedProduct({...selectedProduct, size: newSize, price: selectedSize.price});
+        setSelectedProduct({ ...selectedProduct, size: newSize, price: selectedSize.price });
     };
 
-    // Funktion zur Behandlung der Mengenänderung
+    // Function for handling quantity change
     const handleQuantityChange = (event) => {
-        const newQuantity = Number(event.target.value); // Neue Menge aus dem Ereignis
-        setSelectedProduct({...selectedProduct, quantity: newQuantity}); // Setze die neue Menge für das ausgewählte Produkt
+        const newQuantity = Number(event.target.value); // New quantity from the event
+        setSelectedProduct({ ...selectedProduct, quantity: newQuantity }); // Set the new quantity for the selected product
     };
 
     const handleExtraChange = (event) => {
         setExtras(event.target.value);
     };
 
-    // Funktion zur Behandlung des Entfernens eines Produkts aus dem Warenkorb
+    // Function for handling removing a product from cart
     const handleRemoveFromCart = (index) => {
-        const newCart = [...cart]; // Kopiere den aktuellen Warenkorb
-        newCart.splice(index, 1); // Entferne das Produkt an der angegebenen Indexposition
-        setCart(newCart); // Setze den aktualisierten Warenkorb im Zustand
-        Cookies.set('cart', JSON.stringify(newCart), {expires: 7}); // Speichere den Warenkorb in Cookies mit Ablaufdatum
-        setAlertMessage('Item removed from cart'); // Setze die Benachrichtigungsnachricht
-        setAlertSeverity('info'); // Setze die Benachrichtigungsschweregrad
-        setAlertOpen(true); // Öffne die Benachrichtigung
+        const newCart = [...cart]; // Copy the current cart
+        newCart.splice(index, 1); // Remove the product at the specified index position
+        setCart(newCart); // Set the updated cart in state
+        Cookies.set('cart', JSON.stringify(newCart), { expires: 7 }); // Save the cart in cookies with expiry
+        setAlertMessage('Item removed from cart'); // Set the notification message
+        setAlertSeverity('info'); // Set the notification severity
+        setAlertOpen(true); // Open the notification
     };
 
-    // Funktion zur Darstellung der Warenkorbartikel
+    // Function for rendering cart items
     const renderCartItems = () => {
         if (cart.length === 0) {
             return <ListItem>
@@ -158,7 +165,7 @@ const ProductView = () => {
                     <ListItem key={index}>
                         <ListItemIcon>
                             <img src={`${item.imgName}`} alt={`${item.name} Logo`}
-                                 style={{width: 30, height: 30}}/>
+                                 style={{ width: 30, height: 30 }}/>
                         </ListItemIcon>
                         <ListItemText primary={`${item.name} - ${item.size}`}
                                       secondary={`Quantity: ${item.quantity}, Price: $${item.price}`}/>
@@ -170,22 +177,71 @@ const ProductView = () => {
             </List>
         );
     };
+
+    // Function for filtering drinks based on search query
+    const filterDrinks = (drinks) => {
+        return drinks.filter(drink => {
+            // If no search query present, show all drinks
+            if (!searchQuery) {
+                return true;
+            }
+            // Check if drink name contains search query (case-insensitive)
+            return drink.name.toLowerCase().includes(searchQuery.toLowerCase());
+        });
+    };
+
+    // Function to toggle search bar visibility
+    const handleSearchButtonClick = () => {
+        setIsSearchVisible(prevState => !prevState);
+        handleResetSearch();
+    };
+
+    // Function to reset search query
+    const handleResetSearch = () => {
+        setSearchQuery("");
+    };
+
+    // Handler function for changing search query
+    const handleSearchInputChange = (event) => {
+        setSearchQuery(event.target.value);
+    };
+
+    // Apply filter to displayed drinks
+    const filteredDrinks = filterDrinks(uniqueDrinks);
+
     return (
         <Container maxWidth={false}>
-            <Tabs value={value} onChange={(event, newValue) => setValue(newValue)} centered>
-                <Tab label="All Drinks"/>
-            </Tabs>
+            <Grid>
+                <Tabs value={value} onChange={(event, newValue) => setValue(newValue)} centered>
+                    <Tab label="All Drinks"/>
+                </Tabs>
+                <Grid container justifyContent="flex-end" alignItems="center" spacing={2}>
+                    <IconButton onClick={handleSearchButtonClick}>
+                        <SearchIcon/>
+                    </IconButton>
+                </Grid>
+                {isSearchVisible && (
+                    <TextField
+                        label="Search drinks"
+                        variant="outlined"
+                        value={searchQuery}
+                        onChange={handleSearchInputChange}
+                        fullWidth
+                        style={{ marginTop: 10, marginBottom: 10 }}
+                    />
+                )}
+            </Grid>
             <Grid container spacing={2}>
-                {uniqueDrinks.filter(drink => drink.categoryId === value || value === 0).map((drink) => (
+                {filteredDrinks.filter(drink => drink.categoryId === value || value === 0).map((drink) => (
                     <Grid item xs={12} sm={6} md={3} key={drink.productId}>
                         <Card>
                             <CardActionArea
                                 onClick={() => drink.quantity > 0 ? handleOpenBottomSheet(drink) : null}
                                 disabled={drink.quantity === 0}
-                                style={{opacity: drink.quantity === 0 ? 0.5 : 1}}
+                                style={{ opacity: drink.quantity === 0 ? 0.5 : 1 }}
                             >
                                 <CardMedia
-                                    style={{height: 200, width: '100%', objectFit: 'contain'}}
+                                    style={{ height: 200, width: '100%', objectFit: 'contain' }}
                                     component="img"
                                     image={`/images/products/${drink.imgName}`}
                                     alt={drink.name}
@@ -206,11 +262,11 @@ const ProductView = () => {
                 onClose={handleCloseBottomSheet}>
                 <List>
                     <ListItem>
-                        <ListItemAvatar sx={{fontSize: 100}}>
+                        <ListItemAvatar sx={{ fontSize: 100 }}>
                             <Avatar
                                 src={`${selectedProduct.imgName}`}
                                 alt={selectedProduct.name}
-                                sx={{width: 128, height: 128}}  // Stil für größeren Avatar
+                                sx={{ width: 128, height: 128 }}  // Style for larger Avatar
                             />
                         </ListItemAvatar>
                     </ListItem>
@@ -261,15 +317,46 @@ const ProductView = () => {
                         <Button onClick={handleAddToCart} color="primary" variant="contained" fullWidth>Add to
                             Cart</Button>
                     </ListItem>
+                    <ListItem>
+                        <div>
+                            <Accordion style={{ width: "98.3vw", height: "auto" }}>
+                                <AccordionSummary expandIcon={<ExpandMoreIcon/>}
+                                                  id="panel1-header"
+                                                  aria-controls="panel1-content"
+                                >
+                                    <Typography>Ingredients and allergens</Typography>
+                                </AccordionSummary>
+                                <AccordionDetails>
+                                    <Typography>
+                                        Ingredients: {selectedProduct.ingredients} <br/>
+                                        Allergens: {selectedProduct.allergens}
+                                    </Typography>
+                                </AccordionDetails>
+                            </Accordion>
+                            <Accordion>
+                                <AccordionSummary expandIcon={<ExpandMoreIcon/>}
+                                                  id="panel2-header"
+                                                  aria-controls="panel2-content"
+                                >
+                                    <Typography>Nutritional values</Typography>
+                                </AccordionSummary>
+                                <AccordionDetails>
+                                    <Typography>
+                                        {selectedProduct.nutrition}
+                                    </Typography>
+                                </AccordionDetails>
+                            </Accordion>
+                        </div>
+                    </ListItem>
                 </List>
             </Drawer>
             <Snackbar
                 open={alertOpen}
                 autoHideDuration={6000}
                 onClose={() => setAlertOpen(false)}
-                anchorOrigin={{vertical: 'bottom', horizontal: 'left'}} // Position der Snackbar
+                anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }} // Snackbar position
             >
-                <Alert severity={alertSeverity} sx={{width: '100%'}}>
+                <Alert severity={alertSeverity} sx={{ width: '100%' }}>
                     {alertMessage}
                 </Alert>
             </Snackbar>
@@ -282,9 +369,9 @@ const ProductView = () => {
                     <Button onClick={handleCloseDialog}>Close</Button>
                 </DialogActions>
             </Dialog>
-            <Badge badgeContent={cart.length} color="error" anchorOrigin={{vertical: 'top', horizontal: 'right'}}
-                   sx={{position: 'fixed', right: 25, bottom: 95, zIndex: 1}}>
-                <Fab color="primary" aria-label="cart" style={{position: 'fixed', right: 20, bottom: 50, zIndex: 0}}
+            <Badge badgeContent={cart.length} color="error" anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+                   sx={{ position: 'fixed', right: 25, bottom: 95, zIndex: 1 }}>
+                <Fab color="primary" aria-label="cart" style={{ position: 'fixed', right: 20, bottom: 50, zIndex: 0 }}
                      onClick={handleOpenCardView}>
                     <ShoppingCartOutlinedIcon/>
                 </Fab>
@@ -293,4 +380,3 @@ const ProductView = () => {
     );
 };
 export default ProductView;
-
