3a54ee2ce827676dbf9b4f5479e03152e54f2112
Fynn-Luca Knorr <fknorr@stud.hs-heilbronn.de>
2024-05-16 00:49:38 +0200

    - Fixed grammar mistakes

--- Frontend/src/views/ProductView.js
+++ Frontend/src/views/ProductView.js
@@ -1,5 +1,5 @@
-// Import necessary dependencies from React and MUI library
-import React, { useEffect, useState } from "react";
+// Import necessary dependencies from a React and MUI library
+import React, {useEffect, useState} from "react";
 import axios from 'axios'; // HTTP request library for fetching product data
 import Cookies from 'js-cookie'; // Library for managing cookies
 import {
@@ -41,7 +41,7 @@ import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import DeleteIcon from '@mui/icons-material/Delete';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import SearchIcon from '@mui/icons-material/Search';
-import { useNavigate } from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 
 // Function for displaying product cards and cart
 const ProductView = () => {
@@ -51,13 +51,14 @@ const ProductView = () => {
     const [value, setValue] = useState(0); // State for active tab value
     const [cart, setCart] = useState([]); // State for cart
     const [alertOpen, setAlertOpen] = useState(false); // State for Snackbar (notification)
-    const [alertMessage, setAlertMessage] = useState(''); // State for Snackbar message
+    const [alertMessage, setAlertMessage] = useState(''); // State for a Snackbar message
     const [alertSeverity, setAlertSeverity] = useState('success'); // State for Snackbar severity
-    const [bottomSheetOpen, setBottomSheetOpen] = useState(false); // State for bottom sheet open (selecting size and quantity)
+    const [bottomSheetOpen, setBottomSheetOpen] = useState(false); // State for a bottom sheet open (selecting size
+    // and quantity)
     const [selectedProduct, setSelectedProduct] = useState({}); // State for selected product
     const [openDialog, setOpenDialog] = useState(false); // State for dialog (cart view)
     const [isSearchVisible, setIsSearchVisible] = useState(false); // State for search bar visibility
-    const [searchQuery, setSearchQuery] = useState(''); // State for search query
+    const [searchQuery, setSearchQuery] = useState(''); // State for a search query
     const [extras, setExtras] = useState('');
     const navigate = useNavigate();
     const baseURL = "http://localhost:3000/images/products/"; // Based on your server configuration
@@ -83,20 +84,20 @@ const ProductView = () => {
     const handleOpenCardView = () => {
         if (cart.length === 0) {
             setAlertMessage('Your cart is empty'); // Set error message
-            setAlertSeverity('error'); // Set severity of Snackbar to 'error'
+            setAlertSeverity('error'); // Set an severity of Snackbar to 'error'
             setAlertOpen(true); // Open Snackbar
         } else {
-            navigate('/product/card'); // Navigate to CardView route if cart is not empty
+            navigate('/product/card'); // Navigate to CardView route if a cart is not empty
         }
     };
     const handleCloseDialog = () => setOpenDialog(false); // Close the dialog
 
     // Function for handling adding a product to cart
     const handleAddToCart = () => {
-        const newCart = [...cart, { ...selectedProduct, imgName: `${selectedProduct.imgName}`, extras }];
+        const newCart = [...cart, {...selectedProduct, imgName: `${selectedProduct.imgName}`, extras}];
         console.log(newCart); // Check the new cart
         setCart(newCart);
-        Cookies.set('cart', JSON.stringify(newCart), { expires: 7 });
+        Cookies.set('cart', JSON.stringify(newCart), {expires: 7});
         setAlertMessage('Added drink to cart');
         setAlertSeverity('success');
         setAlertOpen(true);
@@ -104,7 +105,7 @@ const ProductView = () => {
         setExtras(''); // Reset extras
     };
 
-    // Function for handling opening bottom sheet (selecting size and quantity)
+    // Function for handling an opening bottom sheet (selecting size and quantity)
     const handleOpenBottomSheet = (product) => {
         const productWithImage = {
             ...product,
@@ -118,7 +119,7 @@ const ProductView = () => {
         setBottomSheetOpen(true);
     };
 
-    // Function for handling closing bottom sheet
+    // Function for handling a closing bottom sheet
     const handleCloseBottomSheet = () => {
         setBottomSheetOpen(false); // Close bottom sheet
     };
@@ -128,13 +129,13 @@ const ProductView = () => {
         const newSize = event.target.value; // New size from the event
         // Find the selected product with the new size and update it
         const selectedSize = drinks.find(p => p.name === selectedProduct.name && p.size === newSize);
-        setSelectedProduct({ ...selectedProduct, size: newSize, price: selectedSize.price });
+        setSelectedProduct({...selectedProduct, size: newSize, price: selectedSize.price});
     };
 
     // Function for handling quantity change
     const handleQuantityChange = (event) => {
         const newQuantity = Number(event.target.value); // New quantity from the event
-        setSelectedProduct({ ...selectedProduct, quantity: newQuantity }); // Set the new quantity for the selected product
+        setSelectedProduct({...selectedProduct, quantity: newQuantity}); // Set the new quantity for the selected product
     };
 
     const handleExtraChange = (event) => {
@@ -146,7 +147,7 @@ const ProductView = () => {
         const newCart = [...cart]; // Copy the current cart
         newCart.splice(index, 1); // Remove the product at the specified index position
         setCart(newCart); // Set the updated cart in state
-        Cookies.set('cart', JSON.stringify(newCart), { expires: 7 }); // Save the cart in cookies with expiry
+        Cookies.set('cart', JSON.stringify(newCart), {expires: 7}); // Save the cart in cookies with expiry
         setAlertMessage('Item removed from cart'); // Set the notification message
         setAlertSeverity('info'); // Set the notification severity
         setAlertOpen(true); // Open the notification
@@ -165,7 +166,7 @@ const ProductView = () => {
                     <ListItem key={index}>
                         <ListItemIcon>
                             <img src={`${item.imgName}`} alt={`${item.name} Logo`}
-                                 style={{ width: 30, height: 30 }}/>
+                                 style={{width: 30, height: 30}}/>
                         </ListItemIcon>
                         <ListItemText primary={`${item.name} - ${item.size}`}
                                       secondary={`Quantity: ${item.quantity}, Price: $${item.price}`}/>
@@ -185,7 +186,7 @@ const ProductView = () => {
             if (!searchQuery) {
                 return true;
             }
-            // Check if drink name contains search query (case-insensitive)
+            // Check if drink name contains a search query (case-insensitive)
             return drink.name.toLowerCase().includes(searchQuery.toLowerCase());
         });
     };
@@ -196,7 +197,7 @@ const ProductView = () => {
         handleResetSearch();
     };
 
-    // Function to reset search query
+    // Function to reset a search query
     const handleResetSearch = () => {
         setSearchQuery("");
     };
@@ -227,7 +228,7 @@ const ProductView = () => {
                         value={searchQuery}
                         onChange={handleSearchInputChange}
                         fullWidth
-                        style={{ marginTop: 10, marginBottom: 10 }}
+                        style={{marginTop: 10, marginBottom: 10}}
                     />
                 )}
             </Grid>
@@ -238,10 +239,10 @@ const ProductView = () => {
                             <CardActionArea
                                 onClick={() => drink.quantity > 0 ? handleOpenBottomSheet(drink) : null}
                                 disabled={drink.quantity === 0}
-                                style={{ opacity: drink.quantity === 0 ? 0.5 : 1 }}
+                                style={{opacity: drink.quantity === 0 ? 0.5 : 1}}
                             >
                                 <CardMedia
-                                    style={{ height: 200, width: '100%', objectFit: 'contain' }}
+                                    style={{height: 200, width: '100%', objectFit: 'contain'}}
                                     component="img"
                                     image={`/images/products/${drink.imgName}`}
                                     alt={drink.name}
@@ -262,11 +263,11 @@ const ProductView = () => {
                 onClose={handleCloseBottomSheet}>
                 <List>
                     <ListItem>
-                        <ListItemAvatar sx={{ fontSize: 100 }}>
+                        <ListItemAvatar sx={{fontSize: 100}}>
                             <Avatar
                                 src={`${selectedProduct.imgName}`}
                                 alt={selectedProduct.name}
-                                sx={{ width: 128, height: 128 }}  // Style for larger Avatar
+                                sx={{width: 128, height: 128}}  // Style for larger Avatar
                             />
                         </ListItemAvatar>
                     </ListItem>
@@ -319,7 +320,7 @@ const ProductView = () => {
                     </ListItem>
                     <ListItem>
                         <div>
-                            <Accordion style={{ width: "98.3vw", height: "auto" }}>
+                            <Accordion style={{width: "98.3vw", height: "auto"}}>
                                 <AccordionSummary expandIcon={<ExpandMoreIcon/>}
                                                   id="panel1-header"
                                                   aria-controls="panel1-content"
@@ -354,9 +355,9 @@ const ProductView = () => {
                 open={alertOpen}
                 autoHideDuration={6000}
                 onClose={() => setAlertOpen(false)}
-                anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }} // Snackbar position
+                anchorOrigin={{vertical: 'bottom', horizontal: 'left'}} // Snackbar position
             >
-                <Alert severity={alertSeverity} sx={{ width: '100%' }}>
+                <Alert severity={alertSeverity} sx={{width: '100%'}}>
                     {alertMessage}
                 </Alert>
             </Snackbar>
@@ -369,9 +370,9 @@ const ProductView = () => {
                     <Button onClick={handleCloseDialog}>Close</Button>
                 </DialogActions>
             </Dialog>
-            <Badge badgeContent={cart.length} color="error" anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-                   sx={{ position: 'fixed', right: 25, bottom: 95, zIndex: 1 }}>
-                <Fab color="primary" aria-label="cart" style={{ position: 'fixed', right: 20, bottom: 50, zIndex: 0 }}
+            <Badge badgeContent={cart.length} color="error" anchorOrigin={{vertical: 'top', horizontal: 'right'}}
+                   sx={{position: 'fixed', right: 25, bottom: 95, zIndex: 1}}>
+                <Fab color="primary" aria-label="cart" style={{position: 'fixed', right: 20, bottom: 50, zIndex: 0}}
                      onClick={handleOpenCardView}>
                     <ShoppingCartOutlinedIcon/>
                 </Fab>
