07f6aac148ad4136698e5d662f5c08228e497e19
Benet Restelica <benet.restelica@gmail.com>
2024-05-16 09:11:36 +0200

    Merge Fehler

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -12,6 +12,8 @@ import {TablesProvider} from "./model/TablesContext";
 import EmployeeView from "./views/EmployeeView";
 import CustomerStartUpButton from "./views/CustomerView";
 import CardView from "./views/CardView";
+import SettingsView from "./views/SettingsView";
+import ProductSettings from "./views/ProductSettings";
 function App() {
   return (
     <Router>
@@ -27,6 +29,9 @@ function App() {
           <Route path="/customerStart/:tableId" element ={<CustomerStartUpButton />}/>
           <Route path="/product/:tableId" element ={<ProductView />}/>
           <Route path="/product/:tableId/card" element ={<CardView />}/>
+          <Route path="/settings" element={<SettingsView />} />
+          <Route path="/settings/product" element={<ProductSettings />} />
+
         </Routes>
         </TablesProvider>
         {/* Konstante Navigation am unteren Rand */}
