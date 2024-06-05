a08c9e594bb62f4b47f3da85c253c7a8165b9188
Benet Restelica <benet.restelica@gmail.com>
2024-06-06 01:35:06 +0200

    Cookies Fix

--- Frontend/src/views/utils.js
+++ Frontend/src/views/utils.js
@@ -1,17 +1,17 @@
 import Cookies from 'js-cookie';
 
 // Utility to load cart from cookies
-export const loadCartFromCookies = () => {
-    const loadedCart = Cookies.get('cart');
+export const loadCartFromCookies = (tableId) => {
+    const loadedCart = Cookies.get(`cart_${tableId}`);
     return loadedCart ? JSON.parse(loadedCart) : [];
 };
 
 // Utility to save cart to cookies
-export const saveCartToCookies = (cart) => {
-    Cookies.set('cart', JSON.stringify(cart), { expires: 7 });
+export const saveCartToCookies = (tableId, cart) => {
+    Cookies.set(`cart_${tableId}`, JSON.stringify(cart), { expires: 7 });
 };
 
 // Utility to remove cart from cookies
-export const removeCartFromCookies = () => {
-    Cookies.remove('cart');
+export const removeCartFromCookies = (tableId) => {
+    Cookies.remove(`cart_${tableId}`);
 };
