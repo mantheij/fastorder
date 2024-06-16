563c880684fde68c439484be386333d8239f932d
Jan Manthei <manthei.jan@gmail.com>
2024-06-16 10:26:10 +0200

    revert completed orders to open

--- Frontend/src/views/CompletedOrdersView.js
+++ Frontend/src/views/CompletedOrdersView.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { Grid, Typography, Box, Button } from '@mui/material';
+import { Grid, Typography, Box, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle }
+    from '@mui/material';
 import useEmployeeController from '../controller/EmployeeController';
 import { createTheme } from '@mui/material/styles';
 import { blue } from '@mui/material/colors';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { format } from 'date-fns';
-
+import RestoreIcon from '@mui/icons-material/Restore';
 const ClockBar = ({ currentTime }) => {
     return (
         <Box sx={{ background: "linear-gradient(to top, #0383E2, #5DADF0)", height: '56px', width: '100%', position:
@@ -20,6 +21,8 @@ const ClockBar = ({ currentTime }) => {
 
 const CompletedOrdersView = () => {
     const [currentTime, setCurrentTime] = useState(new Date());
+    const [dialogOpen, setDialogOpen] = useState(false);
+    const [actionIndex, setActionIndex] = useState(null);
     const { boxes, setBoxes } = useEmployeeController();
     const navigate = useNavigate();
 
@@ -69,6 +72,33 @@ const CompletedOrdersView = () => {
         },
     });
 
+    const handleDialogOpen = (index) => {
+        setActionIndex(index);
+        setDialogOpen(true);
+    };
+
+    const handleDialogClose = () => {
+        setDialogOpen(false);
+        setActionIndex(null);
+    };
+
+    const handleAction = () => {
+        if (actionIndex !== null) {
+            const orderId = boxes[actionIndex].orderId;
+            const newStatus = 'open';
+            axios.patch(`http://localhost:8080/api/orders/${orderId}/status`, { status: newStatus })
+                .then(response => {
+                    console.log('PATCH erfolgreich:', response.data);
+                    setBoxes(prevBoxes => prevBoxes.filter((_, i) => i !== actionIndex));
+                })
+                .catch(error => {
+                    console.error('Fehler beim PATCH:', error);
+                });
+            setDialogOpen(false);
+            setActionIndex(null);
+        }
+    };
+
     return (
         <div style={{ paddingBottom: '56px', minHeight: 'calc(100vh - 56px)', overflowY: 'auto' }}>
             <ClockBar currentTime={currentTime} />
@@ -111,11 +141,29 @@ const CompletedOrdersView = () => {
 
                                 <Typography dangerouslySetInnerHTML={{ __html: item.text }} sx={{ textShadow:
                                         '0px 2px 4px rgba(0, 0, 0, 0.2)' }} />
+
+                                <Button variant="contained" color="primary" size="small"    style={{ minWidth: '30px',
+                                    minHeight: '30px', padding: '5px' }}onClick={() => handleDialogOpen(index)}>
+                                    <RestoreIcon/>
+                                </Button>
                             </Box>
                         </Grid>
                     ))}
                 </Grid>
             </Box>
+
+            <Dialog open={dialogOpen} onClose={handleDialogClose}>
+                <DialogTitle>Confirmation</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Are you sure you want to reopen this order?
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleAction}>Yes</Button>
+                    <Button onClick={handleDialogClose}>No</Button>
+                </DialogActions>
+            </Dialog>
         </div>
     );
 };
