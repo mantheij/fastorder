cf37a3dc5f5f116fd721eedfe863e8f7dbd2b07b
Niklas Gerstlauer <gerstlauer@stud.hs-heilbronn.de>
2024-06-26 21:55:33 +0200

    added products and pictures

--- Backend/src/main/java/de/hhn/labfastord/models/DataInitializer.java
+++ Backend/src/main/java/de/hhn/labfastord/models/DataInitializer.java
@@ -59,21 +59,59 @@ public class DataInitializer implements CommandLineRunner {
             //add category
             ProductCategory categoryBeer = new ProductCategory("Biere", "Alkoholische und nicht-alkoholische Biere");
             ProductCategory categorySoft = new ProductCategory("Softdrinks", "Softdrinks");
+            ProductCategory categoryWine = new ProductCategory("Weine", "Rot-, Weiß- und Roséweine");
+            ProductCategory categoryJuice = new ProductCategory("Säfte", "Fruchtsäfte und Gemüsesäfte");
+            ProductCategory categoryLongdrinks = new ProductCategory("Longdrinks", "Selbst gemischte Getränke");
             categoryRepository.save(categoryBeer);
             categoryRepository.save(categorySoft);
+            categoryRepository.save(categoryWine);
+            categoryRepository.save(categoryJuice);
+            categoryRepository.save(categoryLongdrinks);
 
             //add produkts
+            List<Product> productList = new ArrayList<>();
             Product product1 = new Product("Cola", 2.50, 50, true, categorySoft,
                     "0.33L", "cola.jpeg", "ingredients", "nutrition", "allergens");
             Product product2 = new Product("Fanta", 2.50, 50, true, categorySoft,
                     "0.33L", "fanta.jpeg", "ingredients", "nutrition", "allergens");
-            Product product3 = new Product("Fanta", 4.00, 50, true, categorySoft,
-                    "0.5L", "fanta.jpeg", "ingredients", "nutrition", "allergens");
-            Product product4 = new Product("Fanta", 5.00, 50, true, categorySoft,
-                    "1L", "fanta.jpeg", "ingredients", "nutrition", "allergens");
-            Product product5 = new Product("Bier", 3.00, 50, true, categoryBeer,
-                    "0.33L", "kellerbier.jpeg", "ingredients", "nutrition", "allergens");
-            productRepository.saveAll(Arrays.asList(product1, product2, product3, product4, product5));
+            productRepository.save(product1);
+            productRepository.save(product2);
+
+            productList.add(new Product("Fanta", 4.00, 50, true, categorySoft,
+                    "0.5L", "fanta.jpeg", "ingredients", "nutrition", "allergens"));
+            productList.add(new Product("Fanta", 5.00, 50, true, categorySoft,
+                    "1L", "fanta.jpeg", "ingredients", "nutrition", "allergens"));
+            productList.add(new Product("Sprite", 5.00, 50, true, categorySoft,
+                    "1L", "sprite.jpeg", "ingredients", "nutrition", "allergens"));
+            productList.add(new Product("Cola Zero", 4.00, 50, true, categorySoft,
+                    "0.5L", "cola_zero.jpeg", "ingredients", "nutrition", "allergens"));
+            productList.add(new Product("Heineken", 3.00, 50, true, categoryBeer,
+                    "0.33L", "heineken.jpeg", "ingredients", "nutrition", "allergens"));
+            productList.add(new Product("Bitburger", 3.00, 50, true, categoryBeer,
+                    "0.33L", "bitburger.jpeg", "ingredients", "nutrition", "allergens"));
+            productList.add(new Product("White wine", 10.00, 30, true, categoryWine,
+                    "0.75L", "white_wine.jpeg", "ingredients", "nutrition", "allergens"));
+            productList.add(new Product("Red wine", 12.00, 20, true, categoryWine,
+                    "0.75L", "red_wine.jpeg", "ingredients", "nutrition", "allergens"));
+            productList.add(new Product("Rosé wine", 11.00, 25, true, categoryWine,
+                    "0.75L", "rose_wine.jpeg", "ingredients", "nutrition", "allergens"));
+            productList.add(new Product("Apple juice", 3.50, 40, true, categoryJuice,
+                    "1L", "apple_juice.jpeg", "ingredients", "nutrition", "allergens"));
+            productList.add(new Product("Orange juice", 4.00, 35, true, categoryJuice,
+                    "1L", "orange_juice.jpeg", "ingredients", "nutrition", "allergens"));
+            productList.add(new Product("Grape juice", 4.50, 25, true, categoryJuice,
+                    "1L", "grape_juice.jpeg", "ingredients", "nutrition", "allergens"));
+            productList.add(new Product("Gin Tonic", 8.00, 50, true, categoryLongdrinks,
+                    "0.33L", "gin_tonic.jpeg", "gin, tonic water, ice", "nutrition", "allergens"));
+            productList.add(new Product("Vodka Lemon", 7.50, 50, true, categoryLongdrinks,
+                    "0.33L", "vodka_lemon.jpeg", "vodka, lemon juice, ice", "nutrition", "allergens"));
+            productList.add(new Product("Whiskey Cola", 8.00, 50, true, categoryLongdrinks,
+                    "0.33L", "whiskey_cola.jpeg", "whiskey, cola, ice", "nutrition", "allergens"));
+            productList.add(new Product("Rum Cola", 7.50, 50, true, categoryLongdrinks,
+                    "0.33L", "rum_cola.jpeg", "rum, cola, ice", "nutrition", "allergens"));
+            productList.add(new Product("Tequila Sunrise", 9.00, 50, true, categoryLongdrinks,
+                    "0.33L", "tequila_sunrise.jpeg", "tequila, orange juice, grenadine, ice", "nutrition", "allergens"));
+            productRepository.saveAll(productList);
 
             //add tables
             Tables table1 = new Tables("1", 1, false, 200, 125);
@@ -93,9 +131,9 @@ public class DataInitializer implements CommandLineRunner {
             tablesList.add(new Tables("12", 2, false, 125, 200));
             tablesList.add(new Tables("13", 2, false, 125, 250));
             tablesList.add(new Tables("14", 2, false, 125, 150));
-            tablesList.add(new Tables("17", 2, false, 125, 120));
             tablesList.add(new Tables("15", 2, false, 200, 120));
             tablesList.add(new Tables("16", 2, false, 200, 120));
+            tablesList.add(new Tables("17", 2, false, 125, 120));
             tablesList.add(new Tables("placeholder", 1, false, 0, 0));
 
             tablesRepository.saveAll(tablesList);
